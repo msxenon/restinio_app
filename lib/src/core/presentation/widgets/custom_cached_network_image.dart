50224ed22c408f4a0976ae5125cfe98c021e0e56
Modi <10958931+msxenon@users.noreply.github.com>
2024-09-18 12:19:13 +0200

    Refactor custom_cached_network_image.dart to use CachedNetworkImageProvider and add image filters

--- lib/src/core/presentation/widgets/custom_cached_network_image.dart
+++ lib/src/core/presentation/widgets/custom_cached_network_image.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cached_network_image_platform_interface/cached_network_image_platform_interface.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:restinio_app/src/core/constants/app_colors.dart';
 import 'package:restinio_app/src/core/di/dependencies_container.dart';
 import 'package:restinio_app/src/core/services/app_cache_manager.dart';
 
@@ -11,11 +14,28 @@ class FirebaseCachedNetworkImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CachedNetworkImage(
-      imageUrl: imageUrl,
+    final imageProvider = CachedNetworkImageProvider(
+      imageUrl,
       cacheManager: DependenciesContainer.get<AppCacheManager>(),
       imageRenderMethodForWeb: ImageRenderMethodForWeb.HttpGet,
-      fit: BoxFit.cover,
+    );
+    return Stack(
+      alignment: Alignment.center,
+      fit: StackFit.expand,
+      children: [
+        ImageFiltered(
+          imageFilter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Image(
+            image: imageProvider,
+            fit: BoxFit.cover,
+            colorBlendMode: BlendMode.darken,
+            color: AppColors.onSurface.withOpacity(0.7),
+          ),
+        ),
+        Image(
+          image: imageProvider,
+        ),
+      ],
     );
   }
 }
